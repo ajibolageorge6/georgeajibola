860d48214ffa2bf827ee3f6a3ba1960774b396ea
ajibolageorge6 <105534628+ajibolageorge6@users.noreply.github.com>
2022-09-09 09:38:18 -0700

    Add files via upload

---
+++ port/js/script.js
@@ -0,0 +1,7 @@
+/*===========================typing animation ===========================*/
+var typed= new Typed(".typing",{
+    strings:["Web Develop","Web Designer","Graphic Designer","Cyber Security Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}) 
